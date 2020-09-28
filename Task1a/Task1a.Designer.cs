ce1d04e00ba9d7b15e582e8ed4b9968bf85f615f
dr3amy <arterterone@gmail.com>
2020-09-28 18:47:50 +0300

    added basic form with color selection

--- Task1a/Task1a.Designer.cs
+++ Task1a/Task1a.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.enableDrawing = new System.Windows.Forms.RadioButton();
+            this.enableFilling = new System.Windows.Forms.RadioButton();
+            this.currentColorPicture = new System.Windows.Forms.PictureBox();
+            this.changeCurrentColor = new System.Windows.Forms.Button();
+            this.workArea = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.currentColorPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.workArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enableDrawing
+            // 
+            this.enableDrawing.AutoSize = true;
+            this.enableDrawing.Checked = true;
+            this.enableDrawing.Location = new System.Drawing.Point(117, 23);
+            this.enableDrawing.Margin = new System.Windows.Forms.Padding(2);
+            this.enableDrawing.Name = "enableDrawing";
+            this.enableDrawing.Size = new System.Drawing.Size(80, 17);
+            this.enableDrawing.TabIndex = 1;
+            this.enableDrawing.TabStop = true;
+            this.enableDrawing.Text = "Рисование";
+            this.enableDrawing.UseVisualStyleBackColor = true;
+            // 
+            // enableFilling
+            // 
+            this.enableFilling.AutoSize = true;
+            this.enableFilling.Location = new System.Drawing.Point(201, 23);
+            this.enableFilling.Margin = new System.Windows.Forms.Padding(2);
+            this.enableFilling.Name = "enableFilling";
+            this.enableFilling.Size = new System.Drawing.Size(68, 17);
+            this.enableFilling.TabIndex = 2;
+            this.enableFilling.Text = "Заливка";
+            this.enableFilling.UseVisualStyleBackColor = true;
+            // 
+            // currentColorPicture
+            // 
+            this.currentColorPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.currentColorPicture.Location = new System.Drawing.Point(285, 11);
+            this.currentColorPicture.Margin = new System.Windows.Forms.Padding(2);
+            this.currentColorPicture.Name = "currentColorPicture";
+            this.currentColorPicture.Size = new System.Drawing.Size(76, 41);
+            this.currentColorPicture.TabIndex = 3;
+            this.currentColorPicture.TabStop = false;
+            // 
+            // changeCurrentColor
+            // 
+            this.changeCurrentColor.Location = new System.Drawing.Point(365, 16);
+            this.changeCurrentColor.Margin = new System.Windows.Forms.Padding(2);
+            this.changeCurrentColor.Name = "changeCurrentColor";
+            this.changeCurrentColor.Size = new System.Drawing.Size(100, 30);
+            this.changeCurrentColor.TabIndex = 4;
+            this.changeCurrentColor.Text = "Изменить цвет";
+            this.changeCurrentColor.UseVisualStyleBackColor = true;
+            this.changeCurrentColor.Click += new System.EventHandler(this.changeCurrentColor_Click);
+            // 
+            // workArea
+            // 
+            this.workArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.workArea.Location = new System.Drawing.Point(29, 56);
+            this.workArea.Margin = new System.Windows.Forms.Padding(2);
+            this.workArea.Name = "workArea";
+            this.workArea.Size = new System.Drawing.Size(582, 355);
+            this.workArea.TabIndex = 5;
+            this.workArea.TabStop = false;
+            // 
+            // Task1a
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(638, 429);
+            this.Controls.Add(this.workArea);
+            this.Controls.Add(this.changeCurrentColor);
+            this.Controls.Add(this.currentColorPicture);
+            this.Controls.Add(this.enableFilling);
+            this.Controls.Add(this.enableDrawing);
+            this.Name = "Task1a";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.currentColorPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.workArea)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton enableDrawing;
+        private System.Windows.Forms.RadioButton enableFilling;
+        private System.Windows.Forms.PictureBox currentColorPicture;
+        private System.Windows.Forms.Button changeCurrentColor;
+        private System.Windows.Forms.PictureBox workArea;
     }
 }
--- Task1a/Task1a.cs
+++ Task1a/Task1a.cs
@@ -12,9 +12,26 @@ namespace raster_algs.Task1a
 {
     public partial class Task1a : Form
     {
+        private bool isNowDrawing = false; //true, если зажата мышка и идет рисование
+        private Pen drawingPen;            //перо текущего рисования
+        private Graphics drawingGraphic;
+        private Point oldMousePos;
         public Task1a()
         {
             InitializeComponent();
+            currentColorPicture.BackColor = Color.Black;                  //изначально выбран черный цвет для рисования
+            workArea.Image = new Bitmap(workArea.Width, workArea.Height); //область для рисования изначально белая
+            drawingGraphic = Graphics.FromImage(workArea.Image);
+            drawingGraphic.FillRectangle(new SolidBrush(Color.White), 0, 0, workArea.Width, workArea.Height);
         }
+
+        private void changeCurrentColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+                currentColorPicture.BackColor = dialog.Color;
+        }
+
+
     }
 }
